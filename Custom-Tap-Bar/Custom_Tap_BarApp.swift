6fc267aceb64e993833f535cee678cabe55e8c54
n_leonov <65124365+n-leonov@users.noreply.github.com>
2022-03-26 00:19:25 +0300

    Initial Commit

---
+++ Custom-Tap-Bar/Custom_Tap_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_Tap_BarApp.swift
+//  Custom-Tap-Bar
+//
+//  Created by Macbook on 3/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Custom_Tap_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
